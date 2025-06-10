e605744e0d1b676bcc8a6f4825586950ce23a557
Colin <colin.diesh@gmail.com>
2025-06-09 22:19:33 -0400

    Misc

--- next.config.js
+++ next.config.js
@@ -1,20 +1,6 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
+export default {
   reactStrictMode: true,
   output: 'export',
   basePath: '/demos/lgv-nextjs',
-  webpack(config, { isServer }) {
-    if (!isServer) {
-      // avoids the LocalFile usage in generic-filehandle, after
-      // generic-filehandle v2.0.6 was converted to esm
-      config.resolve.fallback = {
-        ...config.resolve.fallback,
-        fs: false,
-      }
-    }
-
-    return config
-  },
 }
-
-module.exports = nextConfig
